5275ee20f2946cd30c0e4b2ebb56618ed91d5c8e
Nishant Pandey <pandeynishantsagar@gmail.com>
2025-09-15 13:00:16 +0100

    comit7

--- app/frontend/ui.py
+++ app/frontend/ui.py
@@ -5,7 +5,7 @@ from app.common.logger import get_logger
 from app.common.custom_exception import CustomException
 logger=get_logger(__name__)
 st.set_page_config(page_title="Multi AI Agent",layout="centered")
-st.title("Multi AI Agent using Groq and Tavily")
+st.title("Multi AI Agent")
 system_prompt=st.text_area("Define your AI Agent",height=70)
 selected_model=st.selectbox("Select your AI model",settings.ALLOWED_MODEL_NAMES)
 allow_web_search=st.checkbox("Allow web search")
